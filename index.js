99d0c65b3575c1b4f05f0d40a41749421053cc2f
Gurmeet Singh <gurmeet.ece.111040@gmail.com>
2018-06-21 15:04:43 +0530

    New commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send({hi:'there'})
+    res.send({hi:'there is someone over there!!!'})
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
